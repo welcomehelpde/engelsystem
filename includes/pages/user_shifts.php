881578debe83a6f05fe43736186ae3276bb9cc78
Welcome Help! <info@welcomehelp.de>
2015-10-17 20:00:52 +0200

    correction 2

--- includes/pages/user_shifts.php
+++ includes/pages/user_shifts.php
@@ -532,13 +532,11 @@ function view_user_shifts() {
       $tage = array("Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag");
       if ($thistime % (24 * 60 * 60) == 23 * 60 * 60 && $endtime - $starttime > 24 * 60 * 60) {
         $shifts_table .= "<tr class=\"row-day\"><th class=\"row-header\">";
+		$shifts_table .= $tage[$tag];
         $shifts_table .= date('Y-m-d<b\r />H:i', $thistime);
       } elseif ($thistime % (60 * 60) == 0) {
         $shifts_table .= "<tr class=\"row-hour\"><th>";
         $shifts_table .= date("H:i", $thistime);
-      } else {
-        $shifts_table .= "<tr class=\"row-hour\"><th>";
-        $shifts_table .= $tage[$tag];
       } else {
         $shifts_table .= "<tr><th>";
       }

acdad98c02df3205dfe61249df70079674e58366
Welcome Help! <info@welcomehelp.de>
2015-10-21 00:27:24 +0200

    sunyquetest.de

--- includes/helper/email_helper.php
+++ includes/helper/email_helper.php
@@ -15,7 +15,7 @@ function engelsystem_email_to_user($recipient_user, $title, $message, $not_if_it
 }
 
 function engelsystem_email($address, $title, $message) {
-  return mail($address, $title, $message, "Content-Type: text/plain; charset=UTF-8\r\nFrom: Fluechtlingshilfe Erding <erding@welcomehelp.de>");
+  return mail($address, $title, $message, "Content-Type: text/plain; charset=UTF-8\r\nFrom: Fluechtlingshilfe Erding <erding@sunyquetest.de>");
 }
 
 ?>

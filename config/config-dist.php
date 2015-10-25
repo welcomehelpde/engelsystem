23df747a8fd818731be1f4d37389d346e809b416
Stefan Dietz <dev@stefan-dietz.eu>
2015-10-25 17:40:46 +0100

    added distribution config

---
+++ config/config-dist.php
@@ -0,0 +1,13 @@
+<?php
+// MySQL-Connection Settings
+$config = array(
+    'host' => "",
+    'user' => "",
+    'pw' => "",
+    'db' => ""
+);
+
+// STMP Mailserver Config
+$mailConfig['smtp_host'] = '';
+$mailConfig['smtp_user'] = '';
+$mailConfig['smtp_password'] = '';

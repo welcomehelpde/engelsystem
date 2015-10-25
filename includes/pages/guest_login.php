ec4677ec221a5a8ca319870cf3fb4d9e40a6c971
Anjo Vahldiek-Oberwagner <anjo@vahldiek.info>
2015-10-25 23:04:00 +0100

    email send after successful verification to inform about details of the system and specifics about the location

--- includes/pages/guest_login.php
+++ includes/pages/guest_login.php
@@ -321,17 +321,26 @@ function user_activate_account_controller () {
   
   $token = $_GET['token'];
 
-  $checkQuery = 'SELECT COUNT(`UID`) as `exists`
+  $checkQuery = 'SELECT *
                  FROM `User`
                  WHERE `mailaddress_verification_token` = "' . sql_escape($token) . '"';
-  $checkResult = sql_select_single_cell($checkQuery);
+  $checkResult = sql_select($checkQuery);
 
-  if($checkResult === '1') {
+  // check that validation code exists for user
+  if(isset($checkResult[0])) {
     $confirmSql = 'UPDATE `User`
                    SET `user_account_approved` = "1"
-                   WHERE `mailaddress_verification_token` = "' . sql_escape($token) . '"';
+                   WHERE `UID` = "' . $checkResult[0]['UID'] . '"';
     sql_query($confirmSql);
     success(_('Your account is confirmed now. You might want to log in:'));
+
+    // send introductory eMail
+    $msg = _('Your e-Mail address was successfully verified. We would like to use this oportunity to inform you about the system and how things are run. Please find the details below.');
+    if(file_exists(__DIR__.'../../locale/introduction.txt')) {
+      $msg = readfile(__DIR__.'../../locale/introduction.txt');
+    }
+    engelsystem_email_to_user($checkResult[0], _('Introduction to Engelsystem'), $msg);
+
     redirect(page_link_to('login'));
   }
   else {
